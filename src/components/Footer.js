07225c82f55170f567c708fe3b08f8775e7dc459
Vikash pathak <vikashh.pathak@gmail.com>
2020-02-07 15:48:39 +0530

    Fixed index & App.js page.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,29 +2,29 @@ import React from 'react'
 
 function Footer() {
     return(
-        <footer className="footer-container white-text-container">
-        <div className="container">
-          <div className="row">
-            
-            
-            <div className="col-xs-12">
-              <h3>Laravel Live Punjab</h3>
-              
-              <div className="row">
-                <div className="col-xs-12 col-sm-7">
-                  {/*  <p><small>Website created with <a href="http://www.mashup-template.com/" title="Create website with free html template">Mashup Template</a>/<a href="https://www.unsplash.com/" title="Beautiful Free Images">Unsplash</a></small>
+      <footer class="footer-container white-text-container">
+        <div class="container">
+          <div class="row">
+      
+          
+            <div class="col-xs-12">
+              <h3>{process.env.REACT_APP_WEBSITE_NAME}</h3>
+      
+              <div class="row">
+                <div class="col-xs-12 col-sm-7">
+                  {/* <p><small>Website created with <a href="http://www.mashup-template.com/" title="Create website with free html template">Mashup Template</a>/<a href="https://www.unsplash.com/" title="Beautiful Free Images">Unsplash</a></small>
                   </p> */}
                 </div>
-                <div className="col-xs-12 col-sm-5">
-                  <p className="text-right">
-                    <a href="https://facebook.com/" className="social-round-icon white-round-icon fa-icon" title="">
-                      <i className="fa fa-facebook" aria-hidden="true"></i>
+                <div class="col-xs-12 col-sm-5">
+                  <p class="text-right">
+                    <a href="https://facebook.com/" class="social-round-icon white-round-icon fa-icon" title="">
+                      <i class="fa fa-facebook" aria-hidden="true"></i>
                     </a>
-                    <a href="https://twitter.com/" className="social-round-icon white-round-icon fa-icon" title="">
-                      <i className="fa fa-twitter" aria-hidden="true"></i>
+                    <a href="https://twitter.com/" class="social-round-icon white-round-icon fa-icon" title="">
+                      <i class="fa fa-twitter" aria-hidden="true"></i>
                     </a>
-                    <a href="https://www.linkedin.com/" className="social-round-icon white-round-icon fa-icon" title="">
-                      <i className="fa fa-linkedin" aria-hidden="true"></i>
+                    <a href="https://www.linkedin.com/" class="social-round-icon white-round-icon fa-icon" title="">
+                      <i class="fa fa-linkedin" aria-hidden="true"></i>
                     </a>
                   </p>
                 </div>
